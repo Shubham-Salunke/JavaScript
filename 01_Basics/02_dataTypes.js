ac681cda60a5f941c7400e1ceda2ffa082ec6bfd
Shubham-Salunke <salunke369shubham@gmail.com>
2023-10-02 10:17:44 +0530

    update

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";  //treat all JS code as newer version
+
+//alert(3 + 3)  //we are using nodejs, not browser
+
+console.log(3 
+    + 3)    // Code readability shoud be high
+
+ 
+    
+let name = "Shubham"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// Number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalon value
+// undefined =>  
+// symbol => unique
+
+
+// object
+
+console.log(typeof "shubham"); // string
+console.log(typeof age);  // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
