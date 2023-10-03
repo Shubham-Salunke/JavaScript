847f2ef739981849cb6508a291961979241897c7
Shubham-Salunke <salunke369shubham@gmail.com>
2023-10-03 22:51:28 +0530

    Done

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -22,7 +22,22 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "Shubham" => true
 
 let someNumber = 33
-
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+//****************************** Operation ******************
+
+let value = 3
+
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
